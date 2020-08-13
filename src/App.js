08b1f366633f92688c940402983c9f1a03e2e0e7
Huu Nhan <huunhankirigamer@gmail.com>
2020-08-13 16:06:46 +0700

    Create React app realtime input using state and onChange event

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Binding value</h2>
+        <p>Hello, {this.state.value}</p>
+        <input type="text" onChange={this.handleChange} placeholder="Nhập tên bạn vào đây"/>
+      </div>
+    );
+  }
 }
 
 export default App;
